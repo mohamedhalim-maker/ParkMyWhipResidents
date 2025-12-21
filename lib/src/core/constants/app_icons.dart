7a9e02511480851626468b40eb9b745aae9c8aa8
xXhalemXx <mohamedhalem909@gmail.com>
2025-12-21 10:53:31 +0200

    added app icons

---
+++ lib/src/core/constants/app_icons.dart
@@ -0,0 +1,64 @@
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: appIcons
+//      fonts:
+//       - asset: assets/fonts/app_icons_font.ttf
+import 'package:flutter/widgets.dart';
+
+class AppIcons {
+  AppIcons._();
+
+  static const String _fontFamily = 'appIcons';
+
+  static const IconData add = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData backArrowLong =
+      IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData backIcon = IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData bill = IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData calendar = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData car = IconData(0xe905, fontFamily: _fontFamily);
+  static const IconData change = IconData(0xe906, fontFamily: _fontFamily);
+  static const IconData close = IconData(0xe907, fontFamily: _fontFamily);
+  static const IconData correctCheckMarkWithCircle =
+      IconData(0xe908, fontFamily: _fontFamily);
+  static const IconData correctCheckmark =
+      IconData(0xe909, fontFamily: _fontFamily);
+  static const IconData document = IconData(0xe90a, fontFamily: _fontFamily);
+  static const IconData dottedCircle =
+      IconData(0xe90b, fontFamily: _fontFamily);
+  static const IconData edit = IconData(0xe90c, fontFamily: _fontFamily);
+  static const IconData filter = IconData(0xe90d, fontFamily: _fontFamily);
+  static const IconData forwardIcon = IconData(0xe90e, fontFamily: _fontFamily);
+  static const IconData history = IconData(0xe90f, fontFamily: _fontFamily);
+  static const IconData homeIcon = IconData(0xe910, fontFamily: _fontFamily);
+  static const IconData imageIcon = IconData(0xe911, fontFamily: _fontFamily);
+  static const IconData info = IconData(0xe912, fontFamily: _fontFamily);
+  static const IconData loading = IconData(0xe913, fontFamily: _fontFamily);
+  static const IconData locationIcon =
+      IconData(0xe914, fontFamily: _fontFamily);
+  static const IconData logout = IconData(0xe915, fontFamily: _fontFamily);
+  static const IconData menu = IconData(0xe916, fontFamily: _fontFamily);
+  static const IconData message = IconData(0xe917, fontFamily: _fontFamily);
+  static const IconData notSelectedItem =
+      IconData(0xe918, fontFamily: _fontFamily);
+  static const IconData pen = IconData(0xe919, fontFamily: _fontFamily);
+  static const IconData permitUser = IconData(0xe91a, fontFamily: _fontFamily);
+  static const IconData permit = IconData(0xe91b, fontFamily: _fontFamily);
+  static const IconData profileWithCheckMark =
+      IconData(0xe91c, fontFamily: _fontFamily);
+  static const IconData profile = IconData(0xe91d, fontFamily: _fontFamily);
+  static const IconData searchIcon = IconData(0xe91e, fontFamily: _fontFamily);
+  static const IconData selectedItem =
+      IconData(0xe91f, fontFamily: _fontFamily);
+  static const IconData settings = IconData(0xe920, fontFamily: _fontFamily);
+  static const IconData smileFace = IconData(0xe921, fontFamily: _fontFamily);
+  static const IconData stickMan = IconData(0xe922, fontFamily: _fontFamily);
+  static const IconData termsAndConditions =
+      IconData(0xe923, fontFamily: _fontFamily);
+  static const IconData twoHands = IconData(0xe924, fontFamily: _fontFamily);
+  static const IconData twoUsers = IconData(0xe925, fontFamily: _fontFamily);
+  static const IconData upload = IconData(0xe926, fontFamily: _fontFamily);
+  static const IconData visa = IconData(0xe927, fontFamily: _fontFamily);
+  static const IconData warning = IconData(0xe928, fontFamily: _fontFamily);
+}
